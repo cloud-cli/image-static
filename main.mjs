f9915b6768a5564b32a8d5d989409f090c166f92
Darlan Alves <745095+darlanalves@users.noreply.github.com>
2024-11-24 22:34:39 +0530

    chore: skip favicon

--- main.mjs
+++ main.mjs
@@ -8,15 +8,20 @@ const rootDir = '/home/app/dist';
 
 export default async function (request, response, next) {
   const url = new URL(request.url, 'http://local');
+  
+  if (url.pathname === 'favicon.ico') {
+    return next();
+  }
+  
   const isGet = request.method === "GET";
   const relativePath = normalize(url.pathname);
   const fullPath = join(rootDir, relativePath);
   const showList = (enableIndex && noIndexFile && isGet && request.url === "/") ||
-    (enableIndex && isGet && statSync(fullPath).isDirectory() && !existsSync(join(fullPath, 'index.html')));
+    (enableIndex && isGet && existsSync(fullPath) && statSync(fullPath).isDirectory() && !existsSync(join(fullPath, 'index.html')));
 
   if (showList) {
     const list = await readdir(fullPath, { withFileTypes: true });
-    const files = list.map(({ name }) => `<a href="/${join(relativePath, name)}" title="Open ${name}">${name}</a>`);
+    const files = list.map(({ name }) => `<a href="${join(relativePath, name)}" title="Open ${name}">${name}</a>`);
 
     const html = "<h1>Files at </h1><hr/><nav>" + files.join("<br/>") + "</nav>";
 
