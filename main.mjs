7ee4045d1220e9b575fdafc43784f4e2a8e980d9
Darlan Alves <745095+darlanalves@users.noreply.github.com>
2024-11-24 22:30:20 +0530

    fix: include relative path in links

--- main.mjs
+++ main.mjs
@@ -9,13 +9,14 @@ const rootDir = '/home/app/dist';
 export default async function (request, response, next) {
   const url = new URL(request.url, 'http://local');
   const isGet = request.method === "GET";
-  const fullPath = join(rootDir, normalize(url.pathname));
+  const relativePath = normalize(url.pathname);
+  const fullPath = join(rootDir, relativePath);
   const showList = (enableIndex && noIndexFile && isGet && request.url === "/") ||
     (enableIndex && isGet && statSync(fullPath).isDirectory() && !existsSync(join(fullPath, 'index.html')));
 
   if (showList) {
     const list = await readdir(fullPath, { withFileTypes: true });
-    const files = list.map(({ name }) => `<a href="/${name}" title="Open ${name}">${name}</a>`);
+    const files = list.map(({ name }) => `<a href="/${join(relativePath, name)}" title="Open ${name}">${name}</a>`);
 
     const html = "<h1>Files at </h1><hr/><nav>" + files.join("<br/>") + "</nav>";
 
